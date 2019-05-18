8c74176630b42def7f85a330add26138f2266e09
Asrani <asrani.27@gmail.com>
2019-05-18 22:35:58 +0800

    asdasd

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -59,7 +59,8 @@
       
     <div class="row">
     </div>
-    <center><img class="" src="{{url('LTE/logo1.png')}}">
+    <center>
+      {{-- <img class="" src="{{url('LTE/logo1.png')}}"> --}}
         <h2 class="widget-user-username"><b>APLIKASI BPN SKRIPSI SENNA</b></h2>
         <h4 class="widget-user-desc"><b>Kalimantan Selatan</b></h4></center>
     <div class="row">
